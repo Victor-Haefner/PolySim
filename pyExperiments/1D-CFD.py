d4a58ce99196c4556ed8647cd006a9b3e51314a7
Victor <victor.haefner@gmail.com>
2016-06-28 22:50:02 +0200

    burgers equation

--- pyExperiments/1D-CFD.py
+++ pyExperiments/1D-CFD.py
@@ -5,20 +5,35 @@ import numpy
 from matplotlib import pyplot      
 import time, sys
 
-nx = 41 		# system size
+nx = 161 		# system size
 dx = 2.0/(nx-1) # system scale
-nt = 25    		# timesteps simulated
-dt = .025  		# timestep
-c = 1      		# wavespeed
+nt = 125    		# timesteps simulated
+dt = .0001  		# timestep
+c = 40      		# wavespeed
+vis = 0.1		# viscuosity
 
-# setup system
-u = numpy.ones(nx)
-u[.5/dx : 1.0/dx+1]=2
-pyplot.plot(numpy.linspace(0,2,nx), u); # draw initial state
+def initWave(): # setup system
+	u = numpy.ones(nx)
+	u[.9/dx : 1.0/dx+1]=2
+	pyplot.plot(numpy.linspace(0,2,nx), u); # draw initial state
+	return u
 
-for n in range(nt): # for each timestep
-	un = u.copy()
-	for i in range(1,nx): u[i] = un[i]-un[i]*dt/dx*(un[i]-un[i-1]) # compute new state
-	pyplot.plot(numpy.linspace(0,2,nx), u); # draw current state
-	
+def propagate(u, nonlinear):
+	for n in range(nt): # for each timestep
+		un = u.copy()
+		for i in range(1,nx-1):
+			diffusion = vis*dt/dx/dx*(un[i+1]-2*un[i]+un[i-1])
+			convection = c*dt/dx*(un[i]-un[i-1])
+			if nonlinear: convection *= un[i]
+			u[i] = un[i] - convection + diffusion # compute new state, based on Burgers equation
+		pyplot.plot(numpy.linspace(0,2,nx), u); # draw current state
+		
+
+u = initWave()
+propagate(u, False) # linear
+
+u = initWave()
+propagate(u, True) # non linear
+
+
 pyplot.show()
